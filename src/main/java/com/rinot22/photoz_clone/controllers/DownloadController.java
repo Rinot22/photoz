19fb5d85d3d5351a56b764c99c31d7df868a090d
Darya <darya.martyniuk@student.pk.edu.pl>
2022-05-03 19:55:25 +0200

    change mapping

--- src/main/java/com/rinot22/photoz_clone/controllers/DownloadController.java
+++ src/main/java/com/rinot22/photoz_clone/controllers/DownloadController.java
@@ -4,12 +4,11 @@ import com.rinot22.photoz_clone.model.Photo;
 import com.rinot22.photoz_clone.service.PhotozService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@RequestMapping("/photoz")
 public class DownloadController {
 
     @Autowired
